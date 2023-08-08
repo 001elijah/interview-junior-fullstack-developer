6f0cceba2e7209b7fe47e2796774170b03cd98d4
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-08 12:43:19 +0300

    EDIT: button styles; ADD: read screenWidth, Header Title conditional rendering (passing props from app to header)

--- interview-frontend/src/app/app.component.ts
+++ interview-frontend/src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  public getScreenWidth: any;
+  
+  ngOnInit() {
+      this.getScreenWidth = window.innerWidth;
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
+}
--- interview-frontend/src/app/header/header.component.ts
+++ interview-frontend/src/app/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   title: string = 'City Searcher';
+  @Input() screenWidth: any;
+  ngOnInit() {}
 }
