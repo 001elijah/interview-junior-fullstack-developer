b2573002bafac0a27d95085f4899cf286e94e617
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-09 15:26:06 +0300

    ADD: @nestjs/config for .env, cities controller/module/service, Getnethod

--- interview-backend/src/app.module.ts
+++ interview-backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [CitiesModule],
+  imports: [CitiesModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- interview-backend/src/cities/cities.controller.ts
+++ interview-backend/src/cities/cities.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { CitiesService } from './cities.service';
 
 @Controller('cities')
-export class CitiesController {}
+export class CitiesController {
+  constructor(private readonly citiesService: CitiesService) {}
+  @Get() getCities() {
+    const cities = this.citiesService.getCities();
+    return cities;
+  }
+}
