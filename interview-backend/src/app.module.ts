be25d414155e6c980fb5ce9811b92179617009bc
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-10 11:02:45 +0300

    HEROKU: deploy

--- interview-backend/src/app.module.ts
+++ interview-backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [CitiesModule, ConfigModule.forRoot()],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'front'),
+    }),
+    CitiesModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
