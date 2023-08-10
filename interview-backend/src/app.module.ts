c07901501c38a9513e60f781e88008ec4a090d90
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-10 11:49:40 +0300

    UNDO: heroku deploy commit

--- interview-backend/src/app.module.ts
+++ interview-backend/src/app.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'front'),
-    }),
-    CitiesModule,
-    ConfigModule.forRoot(),
-  ],
+  imports: [CitiesModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
