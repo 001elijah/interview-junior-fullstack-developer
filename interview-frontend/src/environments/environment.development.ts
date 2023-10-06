71ee4fced0c3a44ee1894fb41beeed536f24b620
Illia Zolotukha <illia@luday.se>
2023-10-06 15:26:46 +0300

    UPDATE: README, environment.development.ts, environment.ts

--- interview-frontend/src/environments/environment.development.ts
+++ interview-frontend/src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://salty-shelf-05013-1217defc3014.herokuapp.com/cities',
+    apiUrl: 'https://nestjs-interview-junior-fullstack.onrender.com/cities',
+    // apiUrl: 'https://salty-shelf-05013-1217defc3014.herokuapp.com/cities',
     // apiUrl: 'http://localhost:3000/cities',
     page: 1,
     limit: 5
